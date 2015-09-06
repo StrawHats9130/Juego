2f6c67bceae6ae08fdd19467dcadbeb777e86289
Nichi 2130 <dainichi@me.com>
2015-09-06 04:47:49 -0400

    Added the properties for the 6 recently added classes & changed their accessability to public

--- Project/SuperAdventure/Engine/Moster.cs
+++ Project/SuperAdventure/Engine/Moster.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
-    class Moster
+   public class Moster
     {
-    }
+       public int ID { get; set; }
+       public string Name { get; set; }
+       public int MaximumHitPoints { get; set; }
+       public int CurrentHitPoints { get; set; }
+       public int MaximumDamage { get; set; }
+       public int RewardExperiencePoints { get; set; }
+       public int RewardGold { get; set; }
+   }
 }
