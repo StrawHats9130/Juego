8b6429fa2091fdcbbca0b35396e18554c19f78c0
Nichi 2130 <dainichi@me.com>
2015-09-06 04:20:29 -0400

    Instantiated a Player Object in the SuperAdventure Class. Added some dummy data & binded the Data to the appropriate labels. Ran the program & validated the output to be correct. **note deleting the Test button Click Event didnt remove all the refrences to the object. Also had to delete the values in the SuperAdventure.Designer Class. Finally also went to the Form and removd the blank button.

--- Project/SuperAdventure/SuperAdventure/SuperAdventure.cs
+++ Project/SuperAdventure/SuperAdventure/SuperAdventure.cs
@@ -3,23 +3,34 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Engine;
+
 namespace SuperAdventure
 {
     public partial class SuperAdventure : Form
     {
+        private Player _player;
         public SuperAdventure()
         {
             InitializeComponent();
-        }
+        
+            _player = new Player();
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
 
-        private void btnTest_Click(object sender, EventArgs e)
-        {
-            lblGold.Text = "123";
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString(CultureInfo.CurrentCulture);
+            lblGold.Text = _player.Gold.ToString(CultureInfo.InvariantCulture);
+            lblExperience.Text = _player.ExperiencePoints.ToString(CultureInfo.InvariantCulture);
+            lblLevel.Text = _player.Level.ToString(CultureInfo.InvariantCulture);
         }
     }
 }
